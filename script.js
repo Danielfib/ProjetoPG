349e27444c51d25c75656e2efc5c1b64057b47f5
Daniel Filgueira <dfb2@cin.ufpe.br>
2017-11-05 16:48:38 -0300

    juncao inicial com ramom

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+var av = 1000; //numero de avaliacao(vai ser escolhido pelo usuario)
+var qtdPontosJuncao = 0;
+var points = []; //array dos pontos colocados pelo usuario
+var pointsBezier = []; //pontos de controle de Bezier
+var arrayUs = []; //Us
+var intervals = []; //deltas
+var vetores = []; //vetores
+var alfas = []; //alfas para bessel
+var move = false; //usado no mover ponto
+var index = -1; //o getIndex itera ela, e ela serve pro clique do mouse
+
 //----------------------------SLIDERS----------------------------
 //array com o valor de cada intervalo:
 //inicia os valores iniciais dos us no array:
@@ -26,7 +37,7 @@ function calcUs(){
     //(soma com os anteriores)
     //como o intervalo entre os us é o valor do slider,
     //essa funcao só calcula os valores de cada u
-    for (var i = 0; i < contadorPontos; i++){
+    for (var i = 0; i < qtdPontosJuncao; i++){
         arrayUs[i] = calcU(i);
     }
 }
@@ -39,8 +50,8 @@ function calcU(i) {
     }
     
 }
-//-----------------------------------------------------------------
-//---------------------------TOGGLE--------------------------------
+//---------------------------------------------------------------
+//---------------------------TOGGLE------------------------------
 //variavel que diz o metodo de calculo de derivadas escolhido:
 var isToggle = 0;
 //1 === FMILL   |    0 === Bessel
@@ -52,12 +63,11 @@ function toggle() {
     }
     //console.log(isToggle);
 }
-//-----------------------------------------------------------------
-//--------------------------CHECKBOX-------------------------------
+//---------------------------------------------------------------
+//--------------------------CHECKBOX-----------------------------
 var isChecked = 0;
-//0 === nao marcado   | 1 === marcado
-function check(){
-    
+//0 === nao marcado(aberta)   | 1 === marcado(fechada)
+function check(){    
     if (isChecked === 0){
         isChecked = 1;
         $("#botaoToggle").css("background-color", "#4CAF50");
@@ -68,87 +78,199 @@ function check(){
     //console.log(isChecked);
 }
 //---------------------------------------------------------------
-var av = 1000;//numero de avaliacao(vai ser escolhido pelo usuario)
-var contadorPontos = 0;
-var points = [];//array dos pontos colocados pelo usuario
-var move = false; //usado no mover ponto
-var index = -1; //o getIndex itera ela, e ela serve pro clique do mouse
 
-//array com o valor de cada u:
-var arrayUs = [];
+function calcIntervals() {
+  for(var i = 0; i < arrayUs.length - 1; i++) {
+    intervals[i] = arrayUs[i+1] - arrayUs[i];
+  }
+}
 
+function calcAlfas() {
+  calcIntervals();
+  for(var i = 1; i < qtdPontosJuncao - 1; i++) {
+    alfas[i] = intervals[i-1] / (intervals[i-1] + intervals[i]);
+  }
+}
+
+//baseado no toggle, escolhe o metodo de calculo
+//das derivadas
+function toggleMethod() {
+    if (isToggle === 0){
+        besselTangents();
+    } else {
+        fmillTangents();
+    }
+}
+
+/**
+ * Fechada - Bessel e Fmill
+ * Aberta - Bessel
+ */
+
+function besselTangents() {
+  var vetFirst, nextVet;
+  var constFirst, constNext;
+  var X, Y;
+  var L = qtdPontosJuncao - 1;
+
+  calcAlfas();
+
+  for(var i = 1; i < L; i++) {
+    vetFirst = {x: points[i].x - points[i-1].x, y: points[i].y - points[i-1].y};
+    nextVet = {x: points[i+1].x - points[i].x, y: points[i+1].y - points[i].y};
+    constFirst = (1 - alfas[i]) / intervals[i-1];
+    constNext = alfas[i] / intervals[i];
+    X = constFirst * vetFirst.x + constNext * nextVet.x;
+    Y = constFirst * vetFirst.y + constNext * nextVet.y;
+    vetores[i] = {x: X, y: Y};
+  }
+
+  // calculando os vetores dos extremos
+  if(qtdPontosJuncao > 2) {
+    var vet;
+
+    constFirst = 2 / intervals[0];
+    vet = {x: points[1].x - points[0].x, y: points[1].y - points[0].y};
+    X = constFirst * vet.x - vetores[1].x;
+    Y = constFirst * vet.y - vetores[1].y;
+    vetores[0] = {x: X, y: Y};
+
+    constFirst = 2 / intervals[L-1];
+    vet = {x: points[L].x - points[L-1].x, y: points[L].y - points[L-1].y};
+    X = constFirst * vet.x - vetores[L-1].x;
+    Y = constFirst * vet.y - vetores[L-1].y;
+    vetores[L] = {x: X, y: Y};
+    
+    calcExtremePointsBezir();
+    calcIntermediatePointsBezir();
+  }
+}
+
+function fmillTangents() {
+  var X, Y;
+
+  for(var i = 1; i < qtdPontosJuncao - 1; i++) {
+    X = points[i+1].x - points[i-1].x;
+    Y = points[i+1].y - points[i-1].y;
+    vetores[i] = {x: X, y: Y};
+  }
+}
+
+function calcExtremePointsBezir() {
+  var X, Y;
+  var coef;
+  var L = qtdPontosJuncao - 1;
+
+  coef = intervals[L-1] / 3;
+  X = points[L].x - coef * vetores[L].x;
+  Y = points[L].y - coef * vetores[L].y;
+  pointsBezier[3*L - 1] = {x: X, y: Y};
+
+  coef = intervals[0] / (3 * (intervals[0]));
+  X = points[0].x + coef * vetores[0].x;
+  Y = points[0].y + coef * vetores[0].y;
+  pointsBezier[1] = {x: X, y: Y};
+
+  pointsBezier[0] = {x: points[0].x, y: points[0].y};
+  /*X = //points[0].x + vetores[0].x;
+  Y = //points[0].y + vetores[0].y;
+  pointsBezier[1] = {x: X, y: Y};*/
+  
+  pointsBezier[3*L] = {x: points[L].x, y: points[L].y};
+  /*X = //points[L].x - vetores[L].x;
+  Y = //points[L].y - vetores[L].y;
+  pointsBezier[3*L-1] = {x: X, y: Y};*/
+}
+
+function calcIntermediatePointsBezir() {
+  var X, Y;
+  var coef;
+  var L = qtdPontosJuncao - 1;
+  for (var i = 1; i < L; i++) {
+    coef = intervals[i-1] / (3 * (intervals[i-1] + intervals[i]));
+    X = points[i].x - coef * vetores[i].x;
+    Y = points[i].y - coef * vetores[i].y;
+    pointsBezier[3*i - 1] = {x: X, y: Y};
+
+    pointsBezier[3*i] = {x: points[i].x, y: points[i].y};
+
+    coef = intervals[i] / (3 * (intervals[i-1] + intervals[i]));
+    X = points[i].x + coef * vetores[i].x;
+    Y = points[i].y + coef * vetores[i].y;
+    pointsBezier[3*i + 1] = {x: X, y: Y};
+  }
+}
+
+resizeCanvas();
 
 function resizeCanvas() {
   canvas.width = parseFloat(window.getComputedStyle(canvas).width);
   canvas.height = parseFloat(window.getComputedStyle(canvas).height);
 }
 
-resizeCanvas();
-
 function drawPoints() {
     //antes de desenhar a curva apaga tudo:
-    ctx.clearRect(0, 0, canvas.width, canvas.height);//redesenha o 
-    //desenha todos os pontos
-    for (var i in points) {
-        ctx.beginPath();
-        ctx.arc(points[i].x, points[i].y, 5, 0, 2 * Math.PI);
-        ctx.fillStyle = '#ff8a80';
-        ctx.fill();
-
-        //ligando os pontos
-        if(i > 0){
-            var xAtual = points[i-1].x;
-            var yAtual = points[i-1].y;
-            //deixando a linha tracejada
-            ctx.setLineDash([5 ,3]);
-            ctx.lineWidth = 0.5;
-            ctx.strokeStyle = '#90A4AE';
-            ctx.moveTo(xAtual, yAtual);
-            ctx.lineTo(points[i].x, points[i].y);
-            ctx.stroke();
-        }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);//redesenha o
+  //desenha todos os pontos
+  var cor;
+  for (var i in pointsBezier) {
+    ctx.beginPath();
+    ctx.arc(pointsBezier[i].x, pointsBezier[i].y, 5, 0, 2 * Math.PI);
+    if(i % 3 === 0) {
+      cor = 'red';
+    } else {
+      cor = 'blue';
     }
-    if(contadorPontos > 3) {
-        calcAvaliable();
+    ctx.fillStyle = cor;
+    ctx.fill();
+  }
+  
+  //falta ligar os pontos
+    
+    for(var j = 0; j < qtdPontosJuncao - 1; j++) {
+        var limInf = 3 * j;
+        var limSup = limInf + 3;
+        var array = [];
+        for (var k = limInf; k <= limSup; k++) {
+            array.push({x: pointsBezier[k].x, y: pointsBezier[k].y});
+        }           
+        makeCurve(array);
     }
 }
 
-function calcAvaliable() {
+function makeCurve(array) {
   var pointsCurve = [];
-  //para cada avaliacao:
-  //var t = 1/2;
-  for(var t = 0; t < 1; t = t + 1/av) {
-    //para cada avaliacao:
-    var pointsDeCasteljau = points.slice(0, contadorPontos + 1);
-    for(var n = 1; n < contadorPontos; n++) {
-      //para cada nivel:
-      for(var p = 0; p < contadorPontos - n; p++) {
-        //para cada ponto:
-        var cordX = (1 - t) * pointsDeCasteljau[p].x + t * pointsDeCasteljau[p+1].x;
-        var cordY = (1 - t) * pointsDeCasteljau[p].y + t * pointsDeCasteljau[p+1].y;
-        pointsDeCasteljau[p] = {x: cordX, y: cordY};
-      }
+  for(var t = 0; t <= 1; t = t + 1/av) {
+    var pointsDeCasteljau = [];
+    for(var i = 0; i < array.length; i++) {
+      pointsDeCasteljau.push({x: array[i].x, y: array[i].y});
     }
+    calcAvaliable(pointsDeCasteljau, t);
     pointsCurve.push(pointsDeCasteljau[0]);
   }
   drawCurve(pointsCurve);
 }
 
+function calcAvaliable(pointsDeCasteljau, t) {
+  for(var n = 1; n < pointsDeCasteljau.length; n++) {
+    for(var p = 0; p < pointsDeCasteljau.length - n; p++) {
+      var cordX = (1 - t) * pointsDeCasteljau[p].x + t * pointsDeCasteljau[p+1].x;
+      var cordY = (1 - t) * pointsDeCasteljau[p].y + t * pointsDeCasteljau[p+1].y;
+      pointsDeCasteljau[p] = {x: cordX, y: cordY};
+    }
+  }
+}
+
 function drawCurve(pointsCurve) {
-    console.log();
-  if(contadorPontos > 3) {
-    for(var i in pointsCurve) {
-      ctx.beginPath();
+  for(var i in pointsCurve) {
+    ctx.beginPath();
       
-        if(i > 0) {
-            var xAtual = pointsCurve[i-1].x;
-            var yAtual = pointsCurve[i-1].y;
-            ctx.moveTo(xAtual, yAtual);
-            ctx.strokeStyle = '#0D47A1';
-            ctx.lineWidth = 2;
-            ctx.lineTo(pointsCurve[i].x, pointsCurve[i].y);
-            ctx.stroke();
-        }
+    if(i > 0) {
+      var xAtual = pointsCurve[i-1].x;
+      var yAtual = pointsCurve[i-1].y;
+      ctx.moveTo(xAtual, yAtual);
+      ctx.lineTo(pointsCurve[i].x, pointsCurve[i].y);
+      ctx.stroke();
     }
   }
 }
@@ -167,17 +289,20 @@ function getIndex(click) {
     }
   }
   return -1;
-} 
+}
 
 canvas.addEventListener('mousedown', e => {
   var click = {x: e.offsetX, y: e.offsetY, v:{x: 0, y:0}};
   index = getIndex(click);
   if (index === -1) {
-    contadorPontos = contadorPontos + 1;
+    qtdPontosJuncao = qtdPontosJuncao + 1;
     addSlider();
     points.push(click);
     calcUs();
-    drawPoints();
+    if(qtdPontosJuncao > 2) {
+      toggleMethod();
+      drawPoints();
+    }
   } else {
     move = true;
   }
@@ -188,22 +313,21 @@ canvas.addEventListener('mouseup', e => {
 });
 
 canvas.addEventListener('dblclick', e => {
-    if (index !== -1) {
-        //se o double clique for "emcima" de um ponto valido
-        points.splice(index, 1);
-        contadorPontos--;
-        
-        //para apagar o u:
-        arrayValoresSliders.splice(index, 1);
-        arrayUs.splice(index, 1);
-        calcUs();
-        
-        $("#" +index).remove();
-        //reetiqueta os sliders quando um for removido:
-        for(var i = index; i < contadorPontos+1; i++){
-            $("#" + i).attr('id', i-1);
-        }
+  if (index !== -1) {
+    points.splice(index, 1);
+    qtdPontosJuncao--;
+      
+    //para apagar o u:
+    arrayValoresSliders.splice(index, 1);
+    arrayUs.splice(index, 1);
+    calcUs();
+      
+    $("#" +index).remove();
+    //reetiqueta os sliders quando um for removido:
+    for(var i = index; i < qtdPontosJuncao+1; i++){
+        $("#" + i).attr('id', i-1);
     }
+  }
 });
 
 //mover
@@ -211,13 +335,20 @@ canvas.addEventListener('mousemove', e => {
   if(move){
     var antigo = points[index];
     points[index] = {x: e.offsetX, y: e.offsetY, v:{x:0 , y:0}};
-    points[index].v = {x: e.offsetX - antigo.x, y: e.offsetY - antigo.y};
-    drawPoints();
+    points[index].v = {x: e.offsetX - antigo.x, y: e.offsetY - antigo.y}
+    if(qtdPontosJuncao > 2) {
+      toggleMethod();
+      drawPoints();
+    }
   }     
 });
 
 //intervalo de redesenho do canvas
 //a ultima linha contem a quant de milissegundos entre cada acao
 setInterval(() => {
-  drawPoints();
-}, 1000);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);//redesenha o canvas
+  if(qtdPontosJuncao > 2) {
+    toggleMethod();
+    drawPoints();
+  }
+}, 100);
